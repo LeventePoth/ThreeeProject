9beb154b749a1185dae72f7662d69def09b16317
Levente Poth <levente.poth@gmail>
2019-03-22 14:45:40 +0100

    threeestyle added

--- src/test/java/food/FoodControllerTest.java
+++ src/test/java/food/FoodControllerTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import threee.food.FoodController;
+import threee.food.FoodService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(FoodController.class)
